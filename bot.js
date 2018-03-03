242eadc568379c7b285ef5bc5cfce49e94accd74
AgentPerro <36971456+AgentPerro@users.noreply.github.com>
2018-03-03 01:51:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-bot.user.setPresence({ status: 'online', game: { name: 'OMFG WORKS' } });
+client.user.setPresence({ status: 'online', game: { name: 'OMFG WORKS' } });
 
 client.on('message', message => {
     if (message.content === 'ping') {
