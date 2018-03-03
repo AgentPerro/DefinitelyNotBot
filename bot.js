5bce4fd89976fe4b5efdcb7368b1879cf90383d0
AgentPerro <36971456+AgentPerro@users.noreply.github.com>
2018-03-03 02:31:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 
 client.on('message', message => {
     if(message.content.startsWith("/ping")) {
-            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms")  
-});
+         message.channel.send(new Date().getTime() - message.createdTimestamp + " ms")
     }
+});
 
 client.on('message', message => {
     if (message.content === 'ping') {
