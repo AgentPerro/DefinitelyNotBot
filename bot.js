59251df4977704d8dad091fdbdd8200d6b4b1e40
AgentPerro <36971456+AgentPerro@users.noreply.github.com>
2018-03-10 20:58:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-        var opts = {
-                name: 'Call of Duty: Black Ops 10',
-                url: 'https://www.twitch.tv/lirik',
-                type: 1
-        };
-
-        client.setStatus(null, opts);
-    }
+        client.setStreaming('Call of Duty: Black Ops 10', 'https://www.twitch.tv/lirik', 1);
+        }
 });
 
 client.on('message', message => {
     if(message.content.startsWith("/ping")) {
          message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
-    }
+        }
 });
 
 client.on('message', message => {
